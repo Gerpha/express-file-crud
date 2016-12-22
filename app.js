5a2feca4a07a2be22e3ff28e49c88c2eea47ce2b
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2016-12-22 14:34:24 -0700

    Added function to return books

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.argv[3] || 7070
 const data_store = require('./data_store.js')
-const file = process.argv[2]
 
-load_from_file()
+data_store.load_from_file()
+
+app.get('/api/books', (req, res) => {
+  let data = data_store.get_all_books()
+  res.send(data)
+})
 
 app.listen(port, function() {
   console.log("Listening on port: " + port)
--- data_store.js
+++ data_store.js
@@ -1,24 +1,20 @@
-const path = require('path')
 const fs = require('fs')
 let global = null
 
-module.exports = function load_from_file(){
-  fs.readFile(file, 'utf8', function(err, data) {
-    if (err) {
-      throw err
-    }
-    global = data
+module.exports = {
+
+  load_from_file(){
+    fs.readFile('./db/data.json', 'utf8', function(err, data) {
+      if (err) {
+        throw err
+      }
+      global = data
+    })
+  },
+
+  get_all_books() {
     return global
-  })
+  }
+
+
 }
-
-
-
-
-// fs.readFile(process.argv[2], 'utf8', function(err, data) {
-//   if (err) {
-//     throw err
-//   }
-//   var lines = data.split('\n')
-//   console.log(lines.length - 1)
-// })
